921635dedff560eacb8b695c9f9fa5ca591a59e3
ppyona11 <kah172291@gmail.com>
2025-01-03 00:32:43 +0900

    Initial commit

---
+++ src/main/java/com/project/libmate/LibMateApplication.java
@@ -0,0 +1,13 @@
+package com.project.libmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/libmate/LibMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.libmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
